77848866148aa2bd18d1823ad28520e1f72f8020
Ishwar Kulkarni <ishwark@nvidia.com>
2019-10-04 19:15:00 -0700

    Parameterixed model, mnist types

--- unsupervised_model.py
+++ unsupervised_model.py
@@ -8,16 +8,18 @@ Created on Sat Sep 28 13:31:46 2019
 import torch
 from torch import nn
 from torch.nn import functional as F
+import math
+
 
 class ConvAutoEncoder(nn.Module):
-    def __init__(self):
+    def __init__(self, in_dim=[3, 32, 32], out_dim=[3, 32, 32]):
         super(ConvAutoEncoder, self).__init__()
         
         k_s = [3, 6, 9]
-       # k_s = [ 8, 16, 32 ]
+        k_s = [12, 24, 48]
                
-        self.convEnc = nn.Sequential(
-            nn.Conv2d(3, k_s[0], kernel_size=3, stride=1),
+        self.encoder = nn.Sequential(
+            nn.Conv2d(in_dim[0], k_s[0], kernel_size=3, stride=1),
             nn.ELU(),
             nn.Dropout(),
             
@@ -27,56 +29,72 @@ class ConvAutoEncoder(nn.Module):
             
             nn.Conv2d(k_s[1], k_s[2], kernel_size=5, stride=1),
             nn.ELU(),
+            nn.Dropout(),
              
             nn.Conv2d(k_s[2], 8, kernel_size=3, stride=1),
             nn.Sigmoid()
             )
     
-        self.mu = nn.Linear(392, 64)
         
-        self.var = nn.Linear(392, 64)
-        
-        self.convDec = nn.Sequential(
+        self.decoder = nn.Sequential(
             nn.ConvTranspose2d(1, k_s[0], 3, stride=1),
-            nn.ELU(),
+            nn.ReLU(),
                 
             nn.ConvTranspose2d(k_s[0], k_s[1], 5, stride=1),
-            nn.ELU(),
-                
-            nn.ConvTranspose2d(k_s[1], k_s[1], 5, stride=2),
-            nn.ELU(),
-                
-            nn.ConvTranspose2d(k_s[1], k_s[2], 3, stride=1),
             nn.ReLU(),
+            nn.Dropout(),
+                
+            nn.ConvTranspose2d(k_s[1], k_s[2], 5, stride=2),
+            nn.ReLU(),
+            nn.Dropout(),
             
-            nn.ConvTranspose2d(k_s[2], 1, 3, stride=1),
+            nn.ConvTranspose2d(k_s[2], out_dim[0], 3, stride=1),
             nn.Sigmoid()
             )
+        
+        self.in_lin_sz = self._encoder_out_sz(in_dim)
+        
+        self.out_sz = self._decoder_in_sz(out_dim)
+        
+        out_sz_lin = self.out_sz[0] * self.out_sz[1]
+        
+        self.mu = nn.Linear(self.in_lin_sz, out_sz_lin)
 
+        self.var = nn.Linear(self.in_lin_sz, out_sz_lin)
+        
+    def _encoder_out_sz(self, in_dim):
+        b_im_dim = [1] + in_dim
+        enc = self.encoder(torch.Tensor(torch.ones(b_im_dim)))
+        return enc.view(1, -1).shape[1]
+    
+    def _decoder_in_sz(self, out_dim):
+        out = [out_dim[-2], out_dim[-1]]
+        for d in reversed(self.decoder):
+            if type(d) == nn.ConvTranspose2d:
+                out[0] = (out[0] - math.ceil(d.kernel_size[0]/2))/ d.stride[0]
+                out[1] = (out[1] - math.ceil(d.kernel_size[1]/2))/ d.stride[1]
+ 
+        return [int(i) for i in out]
+        
+    
     def forward(self, x):        
-        enc   = self.convEnc(x)
+        enc   = self.encoder(x)
         encL  = enc.reshape(enc.size(0), -1)        
         muV   = self.mu(encL)
         varV  = self.var(encL)
         
         out   = self._reparameterize(muV, varV)
         
-        lin2d = out.reshape([-1, 1, 8, 8])
+        lin2d = out.reshape([-1, 1, self.out_sz[0], self.out_sz[1]])
         
-        decon = self.convDec(lin2d)
-        out   = decon[:, :, 1:-2, 1:-2]
-    
+        out = self.decoder(lin2d)[:, :, 1:, 1:]
         return out, muV, varV           
         
         print("enc", enc.shape)
-        print("encL", encL.shape)
         print("out", out.shape)
-        print("lin2d", lin2d.shape)
-        print("decon", decon.shape) 
         print("out", out.shape) 
         print("mu", muV.shape)
-        print("var", varV.shape)
-    
+        
     """ https://sergioskar.github.io/Autoencoder/
         These 2 methods from above location
     """    
--- unsupervised_train.py
+++ unsupervised_train.py
@@ -8,6 +8,8 @@ Created on Sat Sep 28 18:52:16 2019
 
 import torch
 import torchvision as tv
+import time
+from torchsummary import summary
 
 from torch.utils.tensorboard import SummaryWriter
 
@@ -15,53 +17,116 @@ from unsupervised_model import ConvAutoEncoder
 from data_loader import CifarLoader
 
 from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
 
-BATCH_SIZE = 1000
-LOG_FREQ   = 50 # in batches
 
-device = torch.device("cuda:0")  if torch.cuda.device_count() >=1 else torch.device("cpu")
-float_dtype = torch.float32
-
-model = ConvAutoEncoder()
-model.to(device).to(float_dtype)
-
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=1e-5)        
-
-train_loader = DataLoader(CifarLoader(),
-                          batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
-test_loader  = DataLoader(CifarLoader(is_test=True), num_workers=2)
-
-with SummaryWriter('runs/auto_enc') as writer:
-    writer.add_graph(model,  torch.Tensor(torch.rand(BATCH_SIZE, 3, 32, 32)).to(device))
-    writer.flush()
-# %%
-
-model.train()
-step = 0
-for epoch in range(0, 100):
-    for bt, batch_label in enumerate(train_loader):
-        batch = batch_label[0].to(device)
-        recon, mu, logvar = model(batch)
+    
+class UnsupervisedTrainer:
+    
+    def __init__(self, 
+                 exp_type   = 'CIFAR',
+                 max_epochs = 100,
+                 log_freq   = 50, # in batches
+                 model_save = 10):
         
-        batch_1c = torch.mean(batch, axis=1, keepdim = True)        
-        bc, kl = model.loss_function(recon, batch_1c, mu, logvar)
-        loss = bc + kl
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if step % LOG_FREQ == 0:
-            print("%d,\t%d:\t%1.5f = %1.5f + %1.5f"%(epoch, step, loss.item(),  bc.item(), kl.item()))
-            writer.add_image('Train-Images', tv.utils.make_grid(batch[0:4]), step)
-            writer.add_image('Train-1C-Images', tv.utils.make_grid(batch_1c[0:4]), step)
-            writer.add_image('Recon-Images', tv.utils.make_grid(recon[0:4]), step)
-            
-            writer.add_scalars('Loss', {'total':loss, 'bc':bc, 'kl': kl}, step)
-            
-            
-        step = step + 1
-    torch.save(model.state_dict(), "epoch_{}.model".format(epoch))
-            
-            
+        super(UnsupervisedTrainer, self).__init__()
+    
+        self.BATCH_SIZE = 1000
+        self.LOG_FREQ   = log_freq# in batches
+        self.MAX_EPOCHS = max_epochs
+        self.MODEL_SAVE = model_save
+        self.EXP_TYPE   = exp_type
+    
+        self.device = torch.device("cuda:0")  if torch.cuda.device_count() >=1 else torch.device("cpu")
+        self.float_dtype = torch.float32
         
+        if self.EXP_TYPE == 'CIFAR':
+            dataset = CifarLoader('./data')
+            im_shape = [3, 32, 32]
+            rec_shape = [1, 32, 32]
+        elif self.EXP_TYPE == 'MNIST-Fashion':
+            dataset = datasets.FashionMNIST('./data', transform=tv.transforms.ToTensor())
+            im_shape = [1, 28, 28]
+            rec_shape = im_shape
+        elif self.EXP_TYPE == 'MNIST':
+            dataset = datasets.MNIST(root='./data', transform=tv.transforms.ToTensor())
+            im_shape = [1, 28, 28]
+            rec_shape = im_shape
+        
+        self.train_loader = DataLoader(dataset, 
+                                       batch_size=self.BATCH_SIZE, shuffle=True, 
+                                       num_workers=2, pin_memory=True)
+        
+        self.model = ConvAutoEncoder(im_shape, rec_shape)
+        self.model.to(self.device).to(self.float_dtype)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-4, weight_decay=1e-5)        
+        self._summarize(im_shape)
+        
+        self._create_testdata()
+        
+    def _create_testdata(self):
+        pass
+        
+    def _summarize(self, im_shape):
+        dummy_size = tuple( [self.BATCH_SIZE] + im_shape)
+        dummy_input = torch.Tensor(torch.ones(dummy_size)).to(self.device)
+        with SummaryWriter('runs/auto_enc/{}'.format(self.EXP_TYPE)) as self.writer:
+            self.writer.add_graph(self.model, dummy_input)
+            self.writer.flush()
+            
+        summary(self.model, tuple(im_shape))
+    
+    # %%
+    def test_step(self):
+        self.model.train(False)
+        
+        self.model.train(True)
+        
+    
+    # %%
+    def train_loop(self):
+        self.model.train()
+        total_steps = len(self.train_loader) * self.MAX_EPOCHS
+        
+        print("Training {} steps with device set to {}".format(total_steps, 
+                                                               self.device))
+        step = 0
+        start = time.time()
+        for epoch in range(0, self.MAX_EPOCHS):
+            for bt, batch_label in enumerate(self.train_loader):
+                
+                batch = batch_label[0].to(self.device)
+                recon, mu, logvar = self.model(batch)
+                
+                progress = step / total_steps
+                batch_1c = torch.mean(batch, axis=1, keepdim = True)        
+                bc, kl = self.model.loss_function(recon, batch_1c, mu, logvar)
+                loss = bc + kl * progress * 2
+        
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+        
+                if step % self.LOG_FREQ == 0:
+                    dur = 1000 * ( (time.time() - start)/ (self.BATCH_SIZE * self.LOG_FREQ) ) 
+                    print("%d,\t%d:\t%1.5f = %1.5f + %1.5f | %2.1f%% | %1.3fms. | %s"%
+                          (epoch, bt, loss.item(),  bc.item(), kl.item(), 
+                           progress*100, dur, '-'*int(progress*40)))
+                    
+                    self.writer.add_image('Train-Images', tv.utils.make_grid(batch[0:4]), step)
+                    self.writer.add_image('Train-1C-Images', tv.utils.make_grid(batch_1c[0:4]), step)
+                    self.writer.add_image('Recon-Images', tv.utils.make_grid(recon[0:4]), step)
+                    
+                    self.writer.add_scalars('Loss', {'total':loss, 'bc':bc, 'kl': kl}, step)
+                    
+                    start = time.time()
+                step = step + 1
+
+            self.test_step()
+            if epoch % self.MODEL_SAVE == 0:
+                torch.save(self.model.state_dict(), "epoch_{}.model".format(epoch))
+        
+        torch.save(self.model.state_dict(), "epoch_final.model")
+
+ut = UnsupervisedTrainer('MNIST')
+ut.train_loop()
